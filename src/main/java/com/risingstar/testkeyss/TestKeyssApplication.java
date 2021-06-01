7af0c4e51dee21641316b0bc44ad6e1e721b9751
小白人 <603312698@qq.com>
2021-06-01 18:15:06 +0800

    小白人 test keysss

---
+++ src/main/java/com/risingstar/testkeyss/TestKeyssApplication.java
@@ -0,0 +1,13 @@
+package com.risingstar.testkeyss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKeyssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKeyssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/risingstar/testkeyss/TestKeyssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risingstar.testkeyss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKeyssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
